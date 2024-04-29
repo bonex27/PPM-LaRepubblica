b866816726485f0b5750312e961be1281fb1842d
Pietro Bonechi <p.bonechi1@gmail.com>
2024-04-29 19:17:08 +0200

    Fixed header width Added js function for; hide navbar or lock it, Hide other

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-
+const scrollThreshold = 255;
 window.onload = function () {
     dateForWeatherWidget();	// Calculate current date for weather widget
 
+    document.querySelector('#mainLogo').classList.add ("hidden")
+    document.querySelector('#btn-subscribe').classList.add('hidden');
 }
-
+window.onscroll = stickyNavbarOnScroll;
 function dateForWeatherWidget() {
     // Get current date with format "Day, Month Nth YYYY"
     const date = new Date();
@@ -20,6 +22,33 @@ function dateForWeatherWidget() {
     document.querySelector('#today-date').innerHTML = `${dayNumber} ${month} ${year} - Aggiornato alle ${hour}.${Minutes}`;
     //document.querySelector('#weather-date-sidenav').innerHTML = `${day}, ${month} ${dayNumber}${cardinalSuffix(dayNumber)} ${year}`;
 }
+
+function stickyNavbarOnScroll() {
+    // If page is scrolled, show navbar as fixed and add logo to it
+    const scroll = document.documentElement.scrollTop || document.body.scrollTop;
+    document.querySelector('.nav-primary-middle').classList.toggle('hidden', scroll > scrollThreshold);
+
+    if (scroll > scrollThreshold) showStickyNavbar();
+    else hideStickyNavbar();
+}
+
+function showStickyNavbar() {
+    document.querySelector('#btn-subscribe').classList.remove ("hidden")
+    document.querySelector('#central-subscribe').classList.add ("hidden")
+
+    document.querySelector('#mainLogo').classList.remove ("hidden")
+    document.querySelector('#sticky-navbar').classList.add('fixed-top');
+
+}
+
+function hideStickyNavbar() {
+     document.querySelector('#sticky-navbar').classList.remove('fixed-top');
+     document.querySelector('#btn-subscribe').classList.add('hidden');
+
+    document.querySelector('#mainLogo').classList.add ("hidden")
+    document.querySelector('#central-subscribe').classList.remove ("hidden")
+}
+
 function hideFooter() {
     if(document.getElementById("rep-page-footer").className.includes("is-open")){
         document.getElementById("rep-page-footer").className = "rep-page-footer";
