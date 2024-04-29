88d87ee7e793dd0a47f68d178d8ac890d8868686
Pietro Bonechi <p.bonechi1@gmail.com>
2024-04-29 15:55:49 +0200

    Added header with logo and nav bar Need create and manage mobile navbar. Need to fix navbar with correct dropdown menu

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+
+window.onload = function () {
+    dateForWeatherWidget();	// Calculate current date for weather widget
+
+}
+
+function dateForWeatherWidget() {
+    // Get current date with format "Day, Month Nth YYYY"
+    const date = new Date();
+    const day = date.toLocaleString('it-IT', {weekday: 'long'});
+    const month = date.toLocaleString('it-IT', {month: 'long'});
+    const dayNumber = date.getDate();
+    const year = date.getFullYear();
+    const hour =date.getHours();
+    const Minutes =date.getMinutes();
+    // Get cardinal suffix for the day number (e.g. 1st, 2nd, 3rd, 4th, ...)
+
+
+    // Set date in weather widget
+    document.querySelector('#today-date').innerHTML = `${dayNumber} ${month} ${year} - Aggiornato alle ${hour}.${Minutes}`;
+    //document.querySelector('#weather-date-sidenav').innerHTML = `${day}, ${month} ${dayNumber}${cardinalSuffix(dayNumber)} ${year}`;
+}
 function hideFooter() {
     if(document.getElementById("rep-page-footer").className.includes("is-open")){
         document.getElementById("rep-page-footer").className = "rep-page-footer";
